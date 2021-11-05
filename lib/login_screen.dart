fef908a0fd9e8ba2906602fd9e7d8c1a5dc1007c
ankurk10 <59817633+ankurk10@users.noreply.github.com>
2021-11-05 23:53:01 +0530

    Created Login Screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -5,6 +5,108 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: Text(
+              "Enter Premium Sound",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+
+          SizedBox(
+            height: 100,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "Name",
+                    labelStyle: TextStyle(
+                      color: Color(0xff7C6BD7),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Color(0xff7C6BD7),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Passwrod",
+                    labelStyle: TextStyle(
+                      color: Color(0xff7C6BD7),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Color(0xff7C6BD7),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 60,
+                ),
+
+                ElevatedButton(
+                  child: Text("Login",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff7C6BD7),
+                    minimumSize: Size(280, 50),
+                  ),
+                  onPressed: ()
+                  {},
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                Text("Forgot Your Password",
+                style: TextStyle(
+                 fontSize: 14,
+                  decoration: TextDecoration.underline,
+                  color: Color(0xff7C6BD7),
+                ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                Text("New Account",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xff7C6BD7),
+                  ),
+                )
+              ],
+
+            ),
+          ),
+
+
+
+
+        ],
+      ),
+    );
   }
 }
--- lib/welcomepage.dart
+++ lib/welcomepage.dart
@@ -10,6 +10,9 @@ class WelcomePage extends StatelessWidget {
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: <Widget>[
+
+
+
           Image(
               image: new AssetImage('assets/images/headphone.png'),
               fit: BoxFit.cover,
@@ -17,13 +20,48 @@ class WelcomePage extends StatelessWidget {
               width: double.infinity,
               alignment: Alignment.center
           ),
+
+
+          Padding(
+            padding: const EdgeInsets.only(top: 380),
+            child: Column(
+              children: [
+                Text("Listen All Day",
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 15,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Text("Enjoy your playlist without interruption, because a full charge lasts up to 22 hours - or up to 40 hours with Pure ANC off.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+
+
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
             child: ButtonTheme(
               height: 40,
               minWidth: 40,
               child: ElevatedButton(
-                child: const Text("CONTINUE",
+                child: const Text("Login",
                   style: TextStyle(
                       color: Colors.white
                   ),
@@ -45,6 +83,8 @@ class WelcomePage extends StatelessWidget {
             ),
           ),
 
+
+
           Padding(
             padding: EdgeInsets.all(120),
             child: ButtonTheme(
@@ -59,7 +99,7 @@ class WelcomePage extends StatelessWidget {
                     ));
 
                   },
-                  child: const Text('Click Me',
+                  child: const Text('Sign Up',
                     style: TextStyle(color: Color(0xff7C6BD7)),
                   ),
                   style: OutlinedButton.styleFrom(
